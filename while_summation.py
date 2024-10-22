ebfbd0621656abe1a89ddcb8c7c3ba21103ab062
YellowDragon5 <merrijac008@alpinesd.org>
2024-10-22 11:16:31 -0600

    Update while_summation.py
    Fixed tabs

--- while_summation.py
+++ while_summation.py
@@ -1,8 +1,9 @@
 num = int(input("Choose a number: "))
-    x=0
-    z=0
-    new=0
-    while z<num:
-        x=x+1
-        new=x+new
-        z=z+1
+x=0
+z=0
+new=0
+while z<num:
+    x=x+1
+    new=x+new
+    z=z+1
+print(new)
