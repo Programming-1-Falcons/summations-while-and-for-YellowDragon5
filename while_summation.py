907905177ac1e264aca09947e4744a29a9360f00
YellowDragon5 <merrijac008@alpinesd.org>
2024-10-22 11:25:02 -0600

    Update while_summation.py
    Added while true

--- while_summation.py
+++ while_summation.py
@@ -1,9 +1,10 @@
-num = int(input(""))
-x=0
-z=0
-new=0
-while z<num:
-    x=x+1
-    new=x+new
-    z=z+1
-print(new)
+while True:
+    num = int(input(""))
+    x=0
+    z=0
+    new=0
+    while z<num:
+        x=x+1
+        new=x+new
+        z=z+1
+    print(new)
