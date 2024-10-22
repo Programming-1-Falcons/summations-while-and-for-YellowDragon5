7a1dcb8ed508fa1fc7f0ec44379552f4d2ab6c82
YellowDragon5 <merrijac008@alpinesd.org>
2024-10-22 11:13:23 -0600

    Update while_summation.py
    Added while code

--- while_summation.py
+++ while_summation.py
@@ -1,1 +1,8 @@
-#while summation code following directions from Readme
+num = int(input("Choose a number: "))
+    x=0
+    z=0
+    new=0
+    while z<num:
+        x=x+1
+        new=x+new
+        z=z+1
